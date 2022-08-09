20eb45b1fe819073804aaced1bd4583c8fa450bb
Łukasz Knapik <l.knapik88@gmail.com>
2022-08-09 17:42:41 +0200

    added geolocation

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import { IoLocationSharp } from 'react-icons/io5';
 import PropTypes from 'prop-types';
@@ -7,15 +7,17 @@ const LandingPage = ({ fetchWeather }) => {
 	const [lat, setLat] = useState('');
 	const [long, setLong] = useState('');
 
-	const getCord = window.navigator.geolocation.getCurrentPosition(
-		(position) => {
-			setLat(position.coords.latitude);
-			setLong(position.coords.longitude);
-		},
-		(err) => alert(err.message)
-	);
+	useEffect(() => {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				setLat(position.coords.latitude);
+				setLong(position.coords.longitude);
+			},
+			(err) => alert(err.message)
+		);
+	}, []);
 
-	console.log(lat, long);
+	const cords = lat + ' ' + long;
 
 	return (
 		<div className='w-full text-center shadow-2xl bg-green-500 sm:border-4 sm:w-4/5 sm:h-[85%] sm:rounded-3xl sm:shadow-2xl sm:m-auto lg:w-3/5 2xl:w-2/5 custom'>
@@ -27,7 +29,9 @@ const LandingPage = ({ fetchWeather }) => {
 				<p className='pt-3'>Let us locate you :) </p>
 				<div className='mx-auto pt-5 hover:cursor-pointer'>
 					<IoLocationSharp
-						onClick={() => getCord}
+						onClick={() =>
+							lat ? fetchWeather(cords) : alert('accept geolocation')
+						}
 						className='text-4xl mx-auto hover:text-green-600'
 					/>
 				</div>
